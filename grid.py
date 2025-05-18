6d5f417efb0662a62c23443cecf8913f51f0c95c
janz <jan.zalar@arnes.si>
2025-05-18 20:37:14 +0200

    working cell

--- grid.py
+++ grid.py
@@ -1,30 +1,78 @@
 from tkinter import Tk, BOTH, Canvas
-from window import Line, Point
+#from window import Line, Point, Window
+
+class Window():
+    def __init__(self, width, height):
+        self.root= Tk()
+        self.root.minsize(width, height)
+        self.root.maxsize(width, height)
+        self.root.title("testni naslov")
+        self.widget = Canvas(self.root, bg = "gray", height = height, width = width)
+        self.widget.pack(fill = BOTH, expand = 1)
+        self.running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.widget.update_idletasks()
+        self.widget.update()
+    def wait_for_close(self):
+        self.running = True
+        while self.running == True:
+            self.redraw()
+        print("window closed")
+    def close(self):
+        self.running = False
+    def draw_line(self, line, fill_color):
+        line.draw(self.widget,  fill_color)
+
+
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+                self.point1.x, self.point1.y,
+                self.point2.x, self.point2.y,
+                fill = fill_color, width = 2
+                )
 
 
 class Cell():
-    def __init__(self, poin1, point2):
+    def __init__(self, window):
         self.has_left_wall = True
         self.has_right_wall = True
-        self.has_top_wall = True
-        self.has_bottom_wall = True
-
-        self._x1 = point1.x
-        self._x2 = point2.x
-        self._y1 = point1.y
-        self._y2 = point2.y
+        self.has_top_wall = False
+        self.has_bottom_wall = False
         
-        self._win = False
+        self.width = 5
 
-    def draw(self, canvas, fill_color):
+
+        self._x1 = -1
+        self._x2 = -1
+        self._y1 = -1
+        self._y2 = -1
+
+        self._win = window
+
+    def draw(self, x, y):
+        self._x1 = x - self.width
+        self._y1 = y + self.width
+        self._x2 = x + self.width
+        self._y2 = y - self.width
         if self.has_left_wall:
-            canvas.create_line(
-                    self.point1.x, self.point1.y,
-                    self.point1.x, self.point2.y,
-                    fill = fill_color,width = 2
-                    )
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x1, self._y2)), "red")
         if self.has_right_wall:
-            canvas.create_line(
-            
-                    )
+            self._win.draw_line(Line(Point(self._x2, self._y1), Point(self._x2, self._y2)), "red")
+        if self.has_top_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x2, self._y1)), "red")
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)), "red")
 
--- window.py
+++ window.py
@@ -1,55 +1,5 @@
-from tkinter import Tk, BOTH, Canvas
-
-class Window():
-    def __init__(self, width, height):
-        self.root= Tk()
-        self.root.minsize(width, height)
-        self.root.maxsize(width, height)
-        self.root.title("testni naslov")
-        self.widget = Canvas(self.root, bg = "gray", height = height, width = width)
-        self.widget.pack(fill = BOTH, expand = 1)
-        self.running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
-
-    def redraw(self):
-        self.widget.update_idletasks()
-        self.widget.update()
-    def wait_for_close(self):
-        self.running = True
-        while self.running == True:
-            self.redraw()
-        print("window closed")
-    def close(self):
-        self.running = False
-    def draw_line(self, line, fill_color):
-        line.draw(self.widget,  fill_color)
-        
-
-
-class Point():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-class Line():
-    def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
-
-    def draw(self, canvas, fill_color):
-        canvas.create_line(
-                self.point1.x, self.point1.y,
-                self.point2.x, self.point2.y,
-                fill = fill_color, width = 2
-                )
-
-
-
-
-
-
-
-
+#from tkinter import Tk, BOTH, Canvas
+from grid import Window, Point, Line, Cell
 
 def main():
     point1 = Point(0, 0)
@@ -57,7 +7,10 @@ def main():
     win = Window(800, 600)
     line = Line(point1, point2)
     win.draw_line(line, "red")
+    #win.wait_for_close()
+    celica1 = Cell(win)
+    celica1.draw(399, 499)
+    celica1.draw(699, 200)
     win.wait_for_close()
-
 main()
 
